f2dc4bc17365e26db3adfb394cba040eb8df2ca2
rk365040 <rknaidu08@gmail.com>
2022-11-28 08:42:09 -0800

    Update HelloServlet.java

--- src/main/java/com/github/maven/example/HelloServlet.java
+++ src/main/java/com/github/maven/example/HelloServlet.java
@@ -8,6 +8,6 @@ import javax.servlet.http.HttpServlet;
 public class HelloServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
-        System.out.println("HelloServlet: You should see this in the Instance Logs :-)");
+        System.out.println("HelloServlet: You should see this in the Instance Log :-)");
     }
 }
